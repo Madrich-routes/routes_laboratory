187a2e14b97a62d7a82a8ab69091d19c80e9695b
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 05:30:13 +0300

    Поправил settings

--- settings.py
+++ settings.py
@@ -1,20 +1,23 @@
 import os
 
+# TODO: добавить cplex, cbc, glpk, scip, ipopt, etc
+# TODO: lkh2 и lkh3
+# TODO: Мб большие буквы? Или маленькие?
 # TODO: И вообще забирать все из переменных среды. А их ставить через мок файл или через .env
 
-CONCORDE_PATH = "/usr/local/bin/concorde"
-LKH2_PATH = "/usr/local/bin/LKH"  # TODO: Мб большие буквы? Или маленькие?
-LKH3_PATH = "/usr/local/bin/LKH"  # TODO: lkh2 и lkh3
-VRP_CLI_PATH = "/usr/local/bin/vrp-cli"  # TODO: пофиксить (пока что его нету :( )
-
-# TODO: добавить cplex, cbc, glpk, scip, ipopt, etc
-
+os.environ["LKH2"] = "/usr/local/bin/LKH2"
 os.environ["LKH"] = "/usr/local/bin/LKH"
 os.environ["CONCORDE"] = "/usr/local/bin/concorde"
-os.environ["VRP_CLI"] = "/usr/local/bin/vrp-cli"  # TODO: солвер раст
+os.environ["VRP_CLI"] = "/usr/local/bin/vrp-cli"
 
-# TODO: будет пересекаться, все поменять!
-PROBLEM_FILE = "/tmp/myroute_lkh.tsp"
-LKH_PAR_FILE = "/tmp/myroute_lkh.tsp"
-VRP_RES_FILE = "/tmp/myroute_lkh.tsp"
+CONCORDE_PATH = os.environ["LKH"]
+LKH3_PATH = os.environ["CONCORDE"]
+VRP_CLI_PATH = os.environ["VRP_CLI"]
 
+HERE_API_KEY = "MOH6CaTy-5eQQmLRStCMT2wNujETT1ld7n8OZwOSGHo"
+
+PROBLEM_FILE = f"/tmp/problem_lkh.{os.getpid()}.tsp"
+LKH_PAR_FILE = f"/tmp/parameters_lkh.{os.getpid()}.par"
+VRP_RES_FILE = f"/tmp/solution_lkh.{os.getpid()}.sol"
+
+REDIS_HOST = '127.0.0.1'
