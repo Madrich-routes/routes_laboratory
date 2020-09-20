282e7469e1822775bfc12f8f5fa4e74e936025d0
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 03:58:55 +0300

    Добавил решение для кругорейса

--- models/problems/base.py
+++ models/problems/base.py
@@ -1,28 +1,13 @@
+import uuid
+from abc import ABC
+
 import numpy as np
 
 
 class BaseRoutingProblem:
     def __init__(self, matrix: np.ndarray):
         self.matrix = matrix
-
-    def lkh_problem(self) -> str:
-        """
-        Сформировать файл проблемы для LKH-решалки
-        """
-        raise NotImplementedError
-
-    def lkh_par(self) -> str:
-        """
-        Сформировать параметры для файла параметров lkh
-        """
-        raise NotImplementedError
-
-    def pragmatic(self) -> str:
-        """
-        Сформулировать проблему pragmatic-формате
-        TODO: мб это все должно быть в классах солверов? Чем в каждую проблему пихать?
-        """
-        raise NotImplementedError
+        self.uuid = uuid.uuid4()
 
     @property
     def size(self) -> int:
@@ -35,8 +20,35 @@ class BaseRoutingProblem:
         return str(self.matrix)
 
     # ---------------------------------- Характеристики проблемы (а они нам нужны?) ---------------------------
+    #
+    # def is_multi_depot(self) -> bool:
+    #     raise NotImplementedError
 
-    def is_multi_depot(self) -> bool:
+
+class RustSolvable(BaseRoutingProblem, ABC):
+    def pragmatic(self) -> str:
+        """
+        Сформулировать проблему pragmatic-формате
+        TODO: мб это все должно быть в классах солверов? Чем в каждую проблему пихать?
+        """
         raise NotImplementedError
 
-    # ---------------------------------- Всякие вспомогательные методы ---------------------------
+
+class LKHSolvable(BaseRoutingProblem, ABC):
+    def lkh_problem(self) -> str:
+        """
+        Сформировать файл проблемы для LKH-решалки
+        """
+        raise NotImplementedError
+
+    def lkh_par(self) -> str:
+        """
+        Сформировать параметры для файла параметров lkh
+        """
+        raise NotImplementedError
+
+
+class WithDelays(BaseRoutingProblem, ABC):
+    @property
+    def delays(self) -> np.ndarray:
+        raise NotImplementedError
--- models/problems/cvrptw.py
+++ models/problems/cvrptw.py
@@ -1,37 +1,50 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 
-from formats.tsplib import dumps_matrix
+from formats import tsplib
+from models.problems.base import BaseRoutingProblem
 
 
-@dataclass
-class MTSPProblem(RoutingProblem):
-    vehicles: int
-    max_len: int
+# @dataclass
+# class MTSPProblem(BaseRoutingProblem):
+#     vehicles: int
+#     max_len: int
+#
+#     def lkh_par(self) -> str:
+#         par = ''
+#         par += 'SPECIAL\n'
+#         par += f'VEHICLES = {self.vehicles}\n'
+#         par += f'DISTANCE = {self.max_len}\n'
+#         par += f'DEPOT = 1\n'
+#         par += f'INITIAL_TOUR_ALGORITHM = MTSP\n'
+#         par += f'MTSP_OBJECTIVE = MINMAX\n'
+#
+#     def pragmatic(self):
+#         ...
 
-    def lkh_par(self) -> str:
-        par = ''
-        par += 'SPECIAL\n'
-        par += f'VEHICLES = {self.vehicles}\n'
-        par += f'DISTANCE = {self.max_len}\n'
-        par += f'DEPOT = 1\n'
-        par += f'INITIAL_TOUR_ALGORITHM = MTSP\n'
-        par += f'MTSP_OBJECTIVE = MINMAX\n'
 
-    def pragmatic(self):
-        ...
-
-
-@dataclass
-class CVRPTWProblem(RoutingProblem):
-    vehicles: int
-    vehicles_capacity: int
-    max_len: int
-    max_hops: int
-
-    time_windows: Tuple[Tuple[int, int], ...]
+class CVRPTWProblem(BaseRoutingProblem):
+    def __init__(
+            self,
+            matrix: np.ndarray,
+            vehicles: int,
+            vehicles_capacity: int,
+            max_len: int,
+            max_hops: int,
+            demands: List[int],
+            time_windows: List[Tuple[int, int]],
+            depot: int = 0,
+    ):
+        super().__init__(matrix)
+        self.vehicles = vehicles
+        self.vehicles_capacity = vehicles_capacity
+        self.max_len = max_len
+        self.max_hops = max_hops
+        self.demands = demands
+        self.time_windows = time_windows
+        self.depot = depot
 
     def lkh_par(self) -> str:
         par = ''
@@ -48,23 +61,21 @@ class CVRPTWProblem(RoutingProblem):
 
     def lkh_problem(self):
         res = '\n'
-        res += r'NAME: CVRPTW',
+        res += f'NAME: CVRPTW-{self.uuid}',
         res += r'TYPE: CVRPTW',
         res += f'DIMENSION: {len(self.matrix)}'
         res += f'VEHICLES: {self.vehicles}'
         res += f'CAPACITY: {self.vehicles_capacity}'
         res += f'DISTANCE: {self.max_len}'
-        res += dumps_matrix(self.matrix)
-        res += 'TIME_WINDOW_SECTION'
-        res += '\n'.join(
-            ' '.join([i, w[0], w[1]]) for i, w in enumerate(self.time_windows)
-        )
-        res += 'DEPOT_SECTION\n'
-        res += f"1"  # TODO: any depot
+        res += tsplib.dumps_matrix(self.matrix)
+        res += tsplib.dump_demands(self.demands)
+        res += tsplib.dumps_time_windows(self.time_windows)
+        res += f'DEPOT_SECTION\n'
+        res += f"{self.depot}"
         res += f"-1"
         res += f"EOF\n",
 
         return res
 
     def pragmatic(self):
-        ...
+        raise NotImplementedError
