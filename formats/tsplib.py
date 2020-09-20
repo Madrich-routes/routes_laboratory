e792b1eba3840f09e1807d25eb61fa2445293786
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 03:58:55 +0300

    Добавил решение для кругорейса

--- formats/tsplib.py
+++ formats/tsplib.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 
@@ -34,5 +34,18 @@ def dumps_time_windows(time_windows: Tuple[Tuple[int, int], ...]):
         [
             ' '.join([str(i), str(w[0]), str(w[1])])
             for i, w in enumerate(time_windows)
+        ]
+    )
+
+
+def dump_demands(demands: List[int]):
+    """
+    Сохраняем спрос в каждой вершине
+    """
+    return '\n'.join(
+        ['DEMAND_SECTION'] +
+        [
+            ' '.join([str(i), str(d)])
+            for i, d in enumerate(demands)
         ]
     )
---
+++ transformers/delays.py
@@ -0,0 +1,36 @@
+"""
+Это модуль с очень простым преобразованием.
+Он прибавляет время пребывания в точке к матрице временн перемещений таким образом,
+чтобы получить эквивалентную задачу без задержек
+"""
+import numpy as np
+
+from models.problems.base import BaseRoutingProblem, WithDelays
+from models.solutions.base import VRPSolution
+from transformers.base import BaseTransformer
+
+
+def remove_delays(matrix: np.ndarray, delays: np.ndarray, restore=False) -> None:
+    """
+    Убираем задержки. Матрица меняется на месте
+    :param restore: восстанавливаеи или удаляем
+    :param matrix: matrix[i][j] — время перемещения i->j
+    :param delays: delays[i] — сколько времени нужно провести в этой точке
+    """
+    if not restore:
+        matrix += delays[:, np.newaxis]
+    else:
+        matrix -= delays[:, np.newaxis]
+
+
+class DelaysRemover(BaseTransformer):
+    def __init__(self):
+        self.delays = None
+
+    def transform(self, problem: WithDelays) -> BaseRoutingProblem:
+        remove_delays(matrix=problem.matrix, delays=problem.delays)
+        return problem
+
+    def restore(self, solution: VRPSolution) -> VRPSolution:
+        solution.problem.matrix = solution.problem
+        return solution
