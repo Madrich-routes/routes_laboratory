ff56721f2c52b8cf4176c8d5950409db69c3fb0a
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 18:02:36 +0300

    Поменял модели

--- customer_cases/eapteka/apeka_addresses.py
+++ customer_cases/eapteka/apeka_addresses.py
@@ -1,4 +1,5 @@
 import json
+
 import pandas as pd
 import requests
 
@@ -8,34 +9,48 @@ def main():
     rows_count = len(data.index)
     res = []
     for i in range(rows_count):
-        address = data.iloc[i]['АдресДоставки']
-        comment = data.iloc[i]['КомментарийКлиента']
+        address = data.iloc[i]["АдресДоставки"]
+        comment = data.iloc[i]["КомментарийКлиента"]
 
-        if str(comment) != 'nan':
+        if str(comment) != "nan":
             geocodeAddress, lot, lan = get_coords(comment)
-            if geocodeAddress == '':
+            if geocodeAddress == "":
                 geocodeAddress, lot, lan = get_coords(address)
         else:
             geocodeAddress, lot, lan = get_coords(address)
 
         res.append([address, comment, geocodeAddress, lot, lan])
         print(f"Обработали уже {i} из {rows_count}")
-    result_df = pd.DataFrame(res,
-                             columns=['Адрес из Excel', 'Коммент из Excel', 'Адрес из геокодера', 'Долгота', 'Широта'])
+    result_df = pd.DataFrame(
+        res,
+        columns=[
+            "Адрес из Excel",
+            "Коммент из Excel",
+            "Адрес из геокодера",
+            "Долгота",
+            "Широта",
+        ],
+    )
     result_df.to_excel("eapteka_result.xlsx")
     i = 0
 
 
 def get_coords(address):
-    map_request = "http://search.maps.sputnik.ru/search/addr?q={address})".format(address=address.replace(" ", "%20"))
+    map_request = "http://search.maps.sputnik.ru/search/addr?q={address})".format(
+        address=address.replace(" ", "%20")
+    )
     response = requests.get(map_request)
     content = json.loads(response.content)
-    geocodeAddress, lot, lan = '', '', ''
+    geocodeAddress, lot, lan = "", "", ""
     if len(content["result"]) > 1:
-        coord = content["result"]["address"][0]["features"][0]["geometry"]["geometries"][0]["coordinates"]
+        coord = content["result"]["address"][0]["features"][0]["geometry"][
+            "geometries"
+        ][0]["coordinates"]
         lot = str(coord[1])
         lan = str(coord[0])
-        geocodeAddress = content["result"]["address"][0]["features"][0]['properties']['display_name']
+        geocodeAddress = content["result"]["address"][0]["features"][0]["properties"][
+            "display_name"
+        ]
     return geocodeAddress, lot, lan
 
 
--- models/graph/distance_matrix.py
+++ models/graph/distance_matrix.py
@@ -2,8 +2,20 @@ import numpy as np
 
 
 class DistanceMatrix:
-    def __init__(self, matrix: np.ndarray) -> None:
+    def __init__(
+            self,
+            matrix: np.ndarray
+    ) -> None:
         self.matrix = matrix
 
     def __len__(self) -> int:
         return len(self.matrix)
+
+    def dist(self):
+        return self.matrix
+
+    def time(
+            self,
+            speed: int,
+    ):
+        return self.matrix / speed
