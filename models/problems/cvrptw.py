16e10526f43a8d5eec839497ba8a7cbbadfda225
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 06:13:11 +0300

    Параметры LKH

--- models/problems/cvrptw.py
+++ models/problems/cvrptw.py
@@ -50,7 +50,7 @@ class CVRPTWProblem(BaseRoutingProblem):
             r'SPECIAL',
             f'VEHICLES = {self.vehicles}',
             # f'DISTANCE = {self.max_len}',
-            f'CAPACITY = {self.vehicles_capacity}',
+            # f'CAPACITY = {self.vehicles_capacity}',
             f'MTSP_MAX_SIZE = {self.max_hops}',
             f'DEPOT = {self.depot}',
             r'INITIAL_TOUR_ALGORITHM = WALK',
