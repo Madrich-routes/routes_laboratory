c0e0e6d37500abae25c1f4508f907913d1d3ee6a
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 14:57:41 +0300

    Добавил eapteka

--- models/agents/job.py
+++ models/agents/job.py
@@ -2,8 +2,12 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Task:
+class Jobs:
     id: int  # индекс в матрице
     tw_start: str  # временные окна
     tw_end: str  # временные окна
     delay: float  # время обслуживания на точке
+    demand: int
+
+    def tw_number(self):
+        ...
