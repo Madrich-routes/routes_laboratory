02658f15d07d374bb908e972253031e418d921f4
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-20 05:22:05 +0300

    Починил формулировку LKH

--- models/problems/cvrptw.py
+++ models/problems/cvrptw.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Tuple, List
 
 import numpy as np
@@ -47,35 +46,32 @@ class CVRPTWProblem(BaseRoutingProblem):
         self.depot = depot
 
     def lkh_par(self) -> str:
-        par = ''
-        par += 'SPECIAL\n'
-        par += f'VEHICLES = {self.vehicles}\n'
-        par += f'DISTANCE = {self.max_len}\n'
-        par += f'CAPACITY = {self.vehicles_capacity}'
-        par += f'MTSP_MAX_SIZE = {self.max_hops}'
-
-        par += f'DEPOT = 1\n'
-        par += f'INITIAL_TOUR_ALGORITHM = WALK\n'
-
-        return par
+        return '\n'.join([
+            r'SPECIAL',
+            f'VEHICLES = {self.vehicles}',
+            f'DISTANCE = {self.max_len}',
+            f'CAPACITY = {self.vehicles_capacity}',
+            f'MTSP_MAX_SIZE = {self.max_hops}',
+            f'DEPOT = {self.depot}',
+            r'INITIAL_TOUR_ALGORITHM = WALK',
+        ])
 
     def lkh_problem(self):
-        res = '\n'
-        res += f'NAME: CVRPTW-{str(self.uuid)}'
-        res += r'TYPE: CVRPTW'
-        res += f'DIMENSION: {len(self.matrix)}'
-        res += f'VEHICLES: {self.vehicles}'
-        res += f'CAPACITY: {self.vehicles_capacity}'
-        res += f'DISTANCE: {self.max_len}'
-        res += tsplib.dumps_matrix(self.matrix)
-        res += tsplib.dump_demands(self.demands)
-        res += tsplib.dumps_time_windows(self.time_windows)
-        res += f'DEPOT_SECTION\n'
-        res += f"{self.depot}"
-        res += f"-1"
-        res += f"EOF\n"
-
-        return res
+        return '\n'.join([
+            f'NAME: CVRPTW-{str(self.uuid)}',
+            r'TYPE: CVRPTW',
+            f'DIMENSION: {len(self.matrix)}',
+            f'VEHICLES: {self.vehicles}',
+            f'CAPACITY: {self.vehicles_capacity}',
+            f'DISTANCE: {self.max_len}',
+            tsplib.dumps_matrix(self.matrix),
+            tsplib.dump_demands(self.demands),
+            tsplib.dumps_time_windows(self.time_windows),
+            f'DEPOT_SECTION',
+            f"{self.depot}",
+            f"-1",
+            f"EOF",
+        ])
 
     def pragmatic(self):
         raise NotImplementedError
