914ec268160276218f5ab6c9d7d28321f2314965
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-21 03:19:48 +0300

    Фикс

--- customer_cases/krugoreys/data_processing/extract_data.py
+++ customer_cases/krugoreys/data_processing/extract_data.py
@@ -10,11 +10,12 @@ from geo.martices.osrm import fix_matrix
 from models.graph.distance_matrix import DistanceMatrix
 from models.rich_vrp.agent import Agent
 from models.rich_vrp.job import Job
-from utils.serialization import read_pickle
+from utils.logs import logger
+from utils.serialization import load_np, save_pickle
 
 
 def load_data():
-    print('Читаем excel...')
+    logger.info('Читаем excel...')
     df = pd.read_excel("../data/pretty_result.xlsx")  # noqa
     df.columns = ['id', "start_time", "end_time", "addr1", "addr2", "coord1", "coord2",
                   's_id', 'e_id', "line_dist", "osrm_dist", "car"]
@@ -45,41 +46,41 @@ def fix_data_errors(df: pd.DataFrame):
 
 
 def build_tasks():
-    print('Создаем таски')
-    gap = timedelta(days=6).seconds
+    logger.info('Создаем таски...')
+    gap = timedelta(days=6).total_seconds()
 
     tasks = []
     for r in tqdm(df.itertuples()):
         tw_start = int((r.start_time - timedelta(days=3)).timestamp())
         tw_end = int((r.end_time + timedelta(days=3)).timestamp())
 
-        if tw_start > tw_end:
+        if tw_start >= tw_end:
             tw_end = tw_start + gap
+            assert tw_start < tw_end, f'Неправильные окна! {tw_start}, {tw_end}'
 
-        if tw_start < tw_end:
-            tasks += [Job(
-                id=int(r.id) - 1,
-                tw_start=tw_start,
-                tw_end=tw_end,
-                delay=int((r.end_time - r.start_time).seconds),
-            )]
+        assert tw_start < tw_end, f'Неправильные окна! {tw_start}, {tw_end}'
+        tasks += [Job(
+            id=int(r.id) - 1,
+            tw_start=tw_start,
+            tw_end=tw_end,
+            delay=int((r.end_time - r.start_time).seconds),
+        )]
 
-    with gzip.open('tasks.pkl.gz', 'wb') as f:
+    logger.info(f'Таски {len(tasks)}')
+    with gzip.open('../big_data/tasks.pkl.gz', 'wb') as f:
         pickle.dump(tasks, file=f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def build_matrix():
-    print('Создаем матрицу...')
+    logger.info('Создаем большую матрицу...')
 
     e_idx = df.e_id.values
     s_idx = df.s_id.values
     res_matrix = small_matrix[e_idx][:, s_idx].astype(np.int32)
-
     d_matr = DistanceMatrix(res_matrix, speed=18.87)
 
-    print('Записываем матрицу на диск...')
-    with gzip.open('matrix.pkl.gz', 'wb', compresslevel=6) as f:
-        pickle.dump(d_matr, file=f, protocol=pickle.HIGHEST_PROTOCOL)
+    logger.info(f'Матрица {d_matr.n}')
+    save_pickle('../big_data/matrix_big.pkl.gz', d_matr)
 
 
 def build_cars():
@@ -106,7 +107,9 @@ def build_cars():
             vehicles += [res]
         except KeyError as e:
             print(e)
-    with gzip.open('vehicles.pkl.gz', 'wb') as f:
+
+    logger.info(f'Машины {len(vehicles)}')
+    with gzip.open('../big_data/vehicles.pkl.gz', 'wb') as f:
         pickle.dump(
             vehicles,
             file=f,
@@ -117,7 +120,7 @@ def build_cars():
 if __name__ == "__main__":
     df = load_data()
     fix_data_errors(df)
-    small_matrix = read_pickle("../data/res_matrix.pkl.gz", compression="gzip")
+    small_matrix = load_np("../big_data/small_matrix.npz")
     coords = pd.read_csv("../data/coordinates.csv", sep=';')[["0", "1"]].values
     small_matrix = fix_matrix(matrix=small_matrix, coords=coords, coeff=1.23)
 
