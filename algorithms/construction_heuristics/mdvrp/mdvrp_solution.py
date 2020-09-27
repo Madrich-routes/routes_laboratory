673a15834ae59871db8dc2fae7e74f30d6fe00a7
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-28 01:40:31 +0300

    Что-то совсем не нужное

--- algorithms/construction_heuristics/mdvrp/mdvrp_solution.py
+++ algorithms/construction_heuristics/mdvrp/mdvrp_solution.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 
 from utils.types import Array
 
@@ -7,10 +8,11 @@ def determine_borderline(
         matrix: Array,
         depots: int,
         jobs: int,
+        candidates: List[List[int]],  # множество ребер-кандидатов
         epsilon: int = 0.7,  # насколько более далекое депо мы рассматриваем
 ):
     """
-    Определяем депо, которые мы вообще рассматриваем.
+    Определяем депо, которые мы вообще рассматриваем для каждого заказчика
     """
     possible_depots = defaultdict(list)
 
---
+++ transformers/to_rpdptw.py
@@ -0,0 +1,42 @@
+"""
+Методы из работы
+Pisinger, Ropke. A general heuristic for vehicle routing problems.
+"""
+
+from dataclasses import dataclass
+from typing import List
+
+from utils.types import Array
+
+
+@dataclass
+class Request:
+    src: int
+    dst: int
+    amount: int
+    tw_start: int
+    tw_end: int
+
+
+@dataclass
+class RPDPTW:
+    requests = List[Request]
+
+def ovrp_to_cvrp(
+        matrix: Array,
+        depots: Array
+):
+    """
+    Откуда угодно до депо бесплатно
+    """
+    matrix[:, depots] = 0
+
+
+def cvrp_to_vrptw(
+        time_matrix: Array,
+        service_times: Array,
+        max_capacity: int,
+):
+    """
+    Cheapest
+    """
