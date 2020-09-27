e62f9a0af63d16a7c8d2ea22594490d36ab3dfd4
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-09-27 14:31:24 +0300

    Добавил метро и много-много всего другого

--- models/rich_vrp/job.py
+++ models/rich_vrp/job.py
@@ -1,17 +1,74 @@
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from functools import cached_property, reduce
+from typing import Set, List
+
+import numpy as np
 
 
 @dataclass
 class Job:
     """
     Класс для описания работы, которую необходимо проделать агенту.
-    Это самый общий класс. В него добавляются любые характеристики
+    Это самый общий класс. В него добавляются любые характеристики.
     """
-    id: int  # индекс в матрице
-    tw_start: datetime  # временные окна
-    tw_end: datetime  # временные окна
+    id: int  # Просто какой-то уникальный id. Индекс в матрице хранят другие классы.  # TODO: а оно нам нужно? uuid?
 
-    delay: timedelta  # время обслуживания на точке
-    demand: int = 0  # количество груза в точке
-    price: int = 1  # награда, получаемая за выполнение этой работы
+    tw_start: int  # временные окна  # TODO: что задавать, если окна не заданы?
+    tw_end: int  # временные окна
+
+    delay: int  # время обслуживания на точке
+    amounts: np.ndarray  # количество разгрузки в точке (той же размерности, что capacities)
+
+    required_skills: Set[int]  # какой набор скилов тут будет необходим
+
+    price: int = 0  # награда, получаемая за выполнение этой работы
+
+    def __le__(self, other):
+        return self.id < other.id
+
+
+class CompositeJob(Job):
+    """
+    Job, который представляет из себ объединение нескольких других джобов.
+    Позволяет склеивать джобы, которые гарантированно нужны вместе.
+    """
+
+    def __init__(self, containers: List[Job]):
+        self.containers = containers[:]
+
+    @cached_property
+    def id(self):
+        return tuple(c.id for c in self.containers)
+
+    @cached_property
+    def tw_start(self):
+        ...  # TODO:
+
+    @cached_property
+    def tw_end(self):
+        ...  # TODO:
+
+    @cached_property
+    def delay(self):
+        ...  # TODO
+
+    @cached_property
+    def amounts(self):
+        """
+        Общая цена всех заказов в списке
+        """
+        return sum(c.amounts for c in self.containers)
+
+    @cached_property
+    def required_skills(self):
+        """
+        Объединение required скилов от всех пацанов
+        """
+        return list(reduce(lambda x, y: x.required_skills | y.required_skills, self.containers, set()))
+
+    @cached_property
+    def price(self):
+        """
+        Общая цена всех заказов в списке
+        """
+        return sum(c.price for c in self.containers)
