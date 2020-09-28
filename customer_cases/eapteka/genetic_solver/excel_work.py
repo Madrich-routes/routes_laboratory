3bd1f3a934178f06075901cc7a1b01b858146609
dmitry.plescheev <dmitry.plescheev@yandex.ru>
2020-09-28 21:06:03 +0300

    update geocoding; refactored

--- customer_cases/eapteka/genetic_solver/excel_work.py
+++ customer_cases/eapteka/genetic_solver/excel_work.py
@@ -5,26 +5,36 @@ import pandas as pd
 from transliterate import translit
 
 
-def save_excel(i, couriers_file, depots_file):
-    writer = pd.ExcelWriter(f'solver_{i}_info.xlsx')
-    couriers = pd.read_csv(couriers_file)
-    depots = pd.read_csv(depots_file)
+def save_excel(name: str, couriers_path: str, depots_path: str, directory='') -> str:
+    """ Generate first excel file with summary info; return file path
+    """
+    file_name = f'{name}_info.xlsx' if not directory else f'{directory}/{name}_info.xlsx'
+    writer = pd.ExcelWriter(file_name)
+    couriers = pd.read_csv(couriers_path)
+    depots = pd.read_csv(depots_path)
     couriers.to_excel(writer, sheet_name='couriers_info', index=False)
     depots.to_excel(writer, sheet_name='depots_info', index=False)
     writer.save()
+    return file_name
 
 
-def save_couriers(i, couriers_directory):
-    writer = pd.ExcelWriter(f'solver_{i}_couriers.xlsx')
+def save_couriers(name: str, couriers_directory: str, directory='') -> str:
+    """ Generate first couriers file; return file path
+    """
+    file_name = f'{name}_couriers.xlsx' if not directory else f'{directory}/{name}_couriers.xlsx'
+    writer = pd.ExcelWriter(file_name)
     for file in os.listdir(couriers_directory):
         if file == '.ipynb_checkpoints' or 'backup' in file:
             continue
         couriers = pd.read_csv(f'{couriers_directory}/{file}')
-        couriers.to_excel(writer, sheet_name=file.replace('Мск,', '')[:31], index=False)
+        couriers.to_excel(writer, sheet_name=file.replace('Мск,', '').replace(' ', '')[:31], index=False)
     writer.save()
+    return file_name
 
 
-def convert_excel(file):
+def convert_excel(file: str):
+    """ Generate summary list in excel info
+    """
     writer = pd.ExcelWriter(file)
     couriers_info = pd.read_excel(file, sheet_name='couriers_info')
     depots_info = pd.read_excel(file, sheet_name='depots_info')
