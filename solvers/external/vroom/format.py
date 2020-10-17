7c5868e287869a1b20e016b605f4fff9114048f4
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-10-17 18:47:40 +0300

    vroom

--- solvers/external/vroom/format.py
+++ solvers/external/vroom/format.py
@@ -1,26 +1,10 @@
 from models.rich_vrp.agent import Agent
 from models.rich_vrp.geometry import BaseGeometry
-from models.rich_vrp.job import Job
 
 
 def export_coords(lat: float, lon: float):
     return [lon, lat]
 
-
-def export_job(job: Job):
-    """
-
-    """
-    return {
-        'id': job.id,
-        'description': str(job),
-        'location_index': ...,  # TODO:
-        'delivery': [],
-        'pickup': [],
-        'skills': job.required_skills,
-        'priority': job.priority,
-        'time_windows': job.time_windows
-    }
 
 
 # def export_break(job: Job):
