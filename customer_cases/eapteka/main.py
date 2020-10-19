9c7b588b1b8b090a632c847e5dee7a156a7ad994
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-10-20 01:59:52 +0300

    ДОбавыил транспорт

--- customer_cases/eapteka/main.py
+++ customer_cases/eapteka/main.py
@@ -8,12 +8,14 @@ logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 TYPE = 'transport_simple_2'
 
-run.run_pharmacy(type_m=TYPE,
-                 time_pharmacy=1,
-                 time_depot=1,
-                 type_weight=15,
-                 type_capacity=30,
-                 driver_weight=200,
-                 driver_capacity=400,
-                 delay=1,
-                 fg='25')
+run.run_pharmacy(
+    type_m=TYPE,
+    time_pharmacy=5,
+    time_depot=10,
+    type_weight=15,
+    type_capacity=30,
+    driver_weight=200,
+    driver_capacity=400,
+    delay=5,
+    fg='25',  # номер результата
+)
