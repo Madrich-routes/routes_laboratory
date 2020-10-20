e72f5285a0fa39447a630a3c0b281e23aebb99d7
evgps <evgps@ya.ru>
2020-10-20 06:15:56 +0300

    works up to orsm

--- geo/transport/settings.py
+++
@@ -1,4 +0,0 @@
-routes_file_pattern = f'data/xls/data-101784-2020-04-08-s*.csv'
-xml_url = 'https://metro.mobile.yandex.net/metro/get_file?file=scheme_1.xml&ver='
-all_uids_file = 'data/all_uids.npy'
-all_pathes_file = "data/all_pathes"
