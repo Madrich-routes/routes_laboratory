d28ff3d95754b02deb53475b706d1bdb43c6d26b
evgps <evgps@ya.ru>
2020-10-20 06:15:56 +0300

    works up to orsm

--- test_transport.py
+++ test_transport.py
@@ -1,7 +1,17 @@
-from geo.transport.matrix import build_dataset_from_files, build_matrix
+from geo.transport.matrix import build_dataset_from_files, build_walk_matrix
+from geo.transport.calc_distance import build_graph
+import pandas as pd
+import numpy as np
+
 
 def main():
     dataset = build_dataset_from_files()
+    dataset.to_pickle('../data/full_df_refactored.pkl')
+    walk_matrix = build_walk_matrix(dataset)
+    dataset.to_pickle('../data/full_df_refactored.pkl')
+    np.savez_compressed('../data/walk_matrix_refactored.npz', walk_matrix=walk_matrix)
+    final_matrix_file = '../data/full_matrix_refactored.npz'
+    final_mat = build_graph(dataset, walk_matrix, final_matrix_file)
 
 if __name__ == "__main__":
     main()
