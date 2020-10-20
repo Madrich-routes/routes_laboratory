d7fabacb948addc3127241579138cc0559bac8f1
evgps <>
2020-10-20 07:27:08 +0500

    complete refactoring

---
+++ geo/transport/settings.py
@@ -0,0 +1,4 @@
+routes_file_pattern = f'data/xls/data-101784-2020-04-08-s*.csv'
+xml_url = 'https://metro.mobile.yandex.net/metro/get_file?file=scheme_1.xml&ver='
+all_uids_file = 'data/all_uids.npy'
+all_pathes_file = "data/all_pathes"
