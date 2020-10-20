f71299137acab293d8e170d99e39c882534adbd4
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-10-20 07:44:43 +0300

    Слил

--- test_transport.py
+++ test_transport.py
@@ -1,8 +1,16 @@
+<<<<<<< Updated upstream
 from geo.transport.matrix import build_dataset_from_files, build_walk_matrix
 from geo.transport.calc_distance import build_graph
 import pandas as pd
 import numpy as np
 import os
+from geo.transport.matrix import build_dataset_from_files
+||||||| merged common ancestors
+from geo.transport.matrix import build_dataset_from_files, build_matrix
+=======
+from geo.transport.matrix import build_dataset_from_files
+
+>>>>>>> Stashed changes
 
 def main():
     if os.path.exists('../data/full_df_refactored.pkl'):
@@ -14,6 +22,8 @@ def main():
     np.savez_compressed('../data/walk_matrix_refactored.npz', walk_matrix=walk_matrix)
     final_matrix_file = '../data/full_matrix_refactored.npz'
     final_mat = build_graph(dataset, walk_matrix, final_matrix_file)
+
+
 
 if __name__ == "__main__":
     main()
