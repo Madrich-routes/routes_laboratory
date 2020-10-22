cb3bf9d7ccc7bb4c9a203ba16b86aa1e323b8f3f
Dmitriy Pleshcheev <dimychspb7@mail.ru>
2020-10-22 17:36:04 +0300

    fixed creating tour

--- solvers/madrich/bindings/problems/vrp_main.cpp
+++ solvers/madrich/bindings/problems/vrp_main.cpp
@@ -1,10 +1,9 @@
 #include <generators.h>
-#include <iostream>
 #include <vrp_problem.h>
 
 
 int main() {
-    auto[vec, couriers, storage, matrix] = generate_vrp(20, 5);
+    auto[vec, couriers, storage, matrix] = generate_vrp(40, 5);
     auto tour = VrpProblem::init_tour(vec, storage, couriers, matrix);
     tour.print();
 }
