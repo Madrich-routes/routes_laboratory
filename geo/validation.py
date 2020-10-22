bb67f02acdd9353eab6b10795833a0e5b57b72ec
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2020-10-22 14:05:59 +0300

    Проверки в OSRM

--- geo/validation.py
+++ geo/validation.py
@@ -10,6 +10,6 @@ def check_distance_matrix(
     """
     Проверяем, что с матрицей расстояний все в порядке
     """
-    assert (matrix < 0).all(), 'В матрице есть отрицательные значения'
+    assert (matrix > 0).all(), 'В матрице есть отрицательные значения'
     assert np.issubdtype(matrix.dtype, np.int32), 'Матрица не приведена к интам'
     assert (line_distance_matrix(points, points) <= matrix).all(), 'Не соблюдается нижнаяя граница'
