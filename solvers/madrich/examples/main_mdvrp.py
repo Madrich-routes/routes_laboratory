ac7eea962df5f52953925263bc2ad5b08967398b
Dmitriy Pleshcheev <dimychspb7@mail.ru>
2020-10-24 01:42:14 +0300

    refactored

--- solvers/madrich/examples/main_mdvrp.py
+++ solvers/madrich/examples/main_mdvrp.py
@@ -4,7 +4,7 @@ from time import time
 
 from solvers.madrich.problems.mdvrp_demo.generators import generate_mdvrp
 from solvers.madrich.problems.mdvrp_demo.mdvrp_problem import ProblemMdvrp
-from solvers.madrich.problems import mdvrp_demo
+from solvers.madrich.problems.mdvrp_demo.operators.improve import improve_tour
 
 faulthandler.enable()
 logging.basicConfig(format='%(message)s', level=logging.INFO)
@@ -16,7 +16,7 @@ tour = problem.init(2, storages, couriers, matrix)
 # draw_mdvrp(storages, tour)
 
 t = time()
-mdvrp_demo.operators.improve.improve_tour(tour, problem, False, False)
+improve_tour(tour, problem, False, False)
 logging.info(f'\nTIME: {time() - t:0.1f} sec')
 
 # draw_mdvrp(storages, tour)
