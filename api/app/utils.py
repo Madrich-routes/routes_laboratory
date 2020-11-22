7b6ea91eafb6a22d7596cde3d0c5d386d2363fd6
Dmitry <dmitry.plescheev@yandex.ru>
2020-11-22 18:24:18 +0300

    api

---
+++ api/app/utils.py
@@ -0,0 +1,20 @@
+import os
+import uuid
+from typing import Optional
+
+
+def save_file(request, path) -> Optional[str]:
+    if 'file' not in request.files:
+        return None
+    file = request.files['file']
+    if file.filename == '':
+        return None
+    if file:
+        filename = str(uuid.uuid4())
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        file.save(os.path.join(path, filename))
+        return filename
+    return None
---
+++ api/test.py
@@ -0,0 +1,20 @@
+import time
+
+import requests
+
+data = open('./data/eapteka/data/update_3.xlsx', 'rb')
+url = f'http://localhost:5000'
+files = {'file': data}
+r = requests.post(f'{url}/solver', files=files)
+job_id = r.json()['job_id']
+print(r.json())
+
+flag, data = True, {}
+while flag:
+    r = requests.get(f'{url}/status', params={'task_id': job_id})
+    time.sleep(2)
+    assert r.status_code == 200, f'fuck, reason: {r.reason}'
+    data = r.json()
+    flag = not data['is_finished']
+assert data['result'], f'bad result from {url}/status'
+print(data['result'])
