4df158912c155534fbdb3f7313bee1e5fc034b28
Dmitriy Pleshcheev <dimychspb7@mail.ru>
2020-11-24 21:34:16 +0300

    prepare agents

--- madrich/solvers/vrp_cli/solver.py
+++ madrich/solvers/vrp_cli/solver.py
@@ -1,12 +1,15 @@
 """В этом модуле находится интерфейс к растовскому солверу."""
 import os
 import uuid
+from copy import deepcopy
 from pathlib import Path
 from typing import Dict, Optional, List, Tuple
 
 import ujson
 
 from madrich import settings
+from madrich.models.rich_vrp.agent import Agent
+from madrich.models.rich_vrp.depot import Depot
 from madrich.models.rich_vrp.plan import Plan
 from madrich.models.rich_vrp.problem import RichVRPProblem, RichMDVRPProblem
 from madrich.models.rich_vrp.solution import VRPSolution, MDVRPSolution
@@ -126,27 +129,58 @@ class RustSolver(BaseSolver):
         solutions = MDVRPSolution(problem)
 
         for sub_problem in problem.sub_problems:  # по сути решаем проблему для каждого депо
+            sub_problem.agents = self._prepare_agents(sub_problem.depot, solutions, problem)
             solution = self.solve(sub_problem)  # запускаем наконец солвер
             solutions.merge(solution)
-            self._cut_windows(solutions, problem)
 
         return solutions
 
     @staticmethod
-    def _cut_windows(solutions: MDVRPSolution, problem: RichMDVRPProblem) -> None:
+    def _prepare_agents(depot: Depot, solutions: MDVRPSolution, problem: RichMDVRPProblem) -> List[Agent]:
         """
-        Нам нужно поменять окна с учетом нового решения после запуска солвера - время курьеров уже занято
+        Нам нужно поменять окна с учетом склада, на котором будем решать задачу, перед запуском солвера
         Нужно учесть, что они ездят между складами - на это тоже уходит время
         """
+        agents = []
+
         for agent in problem.agents:
             new_tw = []
-            for time_window in agent.time_windows:
-                new_tw += RustSolver._cut_window(time_window, solutions.routes[agent.id])
+            for time_window in agent.time_windows:  # создаются новые окна из дефолтных
+                new_tw += RustSolver._cut_window(depot, time_window, solutions.routes[agent.id])
+
+            if not new_tw:
+                continue
+
+            new_agent = deepcopy(agent)
+            new_agent.time_windows = new_tw
+            agents.append(agent)
+
+        return agents
 
     @staticmethod
-    def _cut_window(time_window: Tuple[int, int], plans: List[Plan]) -> List[Tuple[int, int]]:
+    def _cut_window(depot: Depot, time_window: Tuple[int, int], plans: List[Plan]) -> List[Tuple[int, int]]:
         """
         Режем конкретное окно с учетом маршрутов
         """
+        tw = []
 
+        # мы будем искать периоды времени, когда свободен курьер
+        # это что-то такое depot_x [x y] depot_y
+        # ожидается, что мы пытаемся вставить depot_z
+        # depot_x [travel to z] depot_z [travel to y] depot y
+        # и этот период мы должны сократить
+        # со стороны x на between(depot_x, depot_z)
+        # со стороны y на between(depot_z, depot_y)
 
+        start_period: Optional[int] = None  # начало свободного периода
+        prev_depot: Optional[int] = None
+        for plan in plans:
+            if start_period is None:  # ищем начало периода тогда
+                if len(tw) == 0:  # значит это самое начало
+                    start_period = time_window[0]
+                else:
+                    start_period = plan.waypoints[-1].departure  # тогда конец маршрута
+            else:  # ищем конец периода
+        # между depot_x (prev) и depot_y (curr) есть свободное время?
+
+        return tw
